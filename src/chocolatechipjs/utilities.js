07c526a07641e174c1ae359fed4edb7be4ba5f36
Robert Biggs <rbiggs@ymail.com>
2016-11-05 18:34:05 -0700

    Added $.throttle and $.debounce.
    These let you throttle or debounce events to reduce overload.

--- src/chocolatechipjs/utilities.js
+++ src/chocolatechipjs/utilities.js
@@ -328,6 +328,80 @@
         temp += itemsPerPage;
       }
       return ret;
+    },
+
+    /**
+     * Fires an event once during provided wait period. Options are: {leading: true/false, trailing: true/false}.
+     * By default leading is true, meaning that the first event input will fire. Setting leading to false will disable this.
+     * By default trailing is true. Set this to false to disable it.
+     */
+    throttle: function(func, wait, options) {
+      var context, args, result;
+      var timeout = null;
+      var previous = 0;
+      if (!options) options = {};
+      var later = function() {
+        previous = options.leading === false ? 0 : new Date().getTime();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      };
+      return function() {
+        var now = new Date().getTime();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          previous = now;
+          result = func.apply(context, args);
+          if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+          timeout = setTimeout(later, remaining);
+        }
+        return result;
+      }
+    },
+
+    /**
+     * Fires and event once after the designated wait time, regardless of how many other events occurred.
+     * In the case of an input, this will start with the first keypress. After the last keypress and the wait time, the event will fire.
+     * You can make the event fire after the time by passing a third optional truthy argument.
+     */
+    debounce: function(func, wait, immediate) {
+      var timeout, args, context, timestamp, result;
+
+      var later = function() {
+        var last = new Date().getTime() - timestamp;
+
+        if (last < wait && last >= 0) {
+          timeout = setTimeout(later, wait - last);
+        } else {
+          timeout = null;
+          if (!immediate) {
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+          }
+        }
+      };
+
+      return function() {
+        context = this;
+        args = arguments;
+        timestamp = new Date().getTime();
+        var callNow = immediate && !timeout;
+        if (!timeout) timeout = setTimeout(later, wait);
+        if (callNow) {
+          result = func.apply(context, args);
+          context = args = null;
+        }
+
+        return result;
+      };
     }
   });
 
